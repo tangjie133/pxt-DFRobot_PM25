442ff4ab139e7a605817ff7612ee94164332aa57
TgJe <51323906+tangjie133@users.noreply.github.com>
2021-03-22 15:02:33 +0800

    更新 main.blocks, main.ts, test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,7 @@
-// 在此处测试；当此软件包作为插件使用时，将不会编译此软件包。
+custom.setPing(SerialPin.P1, SerialPin.P8)
+basic.forever(function () {
+    serial.writeValue("x", custom.methanal())
+    serial.writeValue("y", custom.pm25())
+    serial.writeValue("z", custom.temperature())
+    serial.writeValue("g", custom.humidity())
+})
